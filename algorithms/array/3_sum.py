ff3145ccb0039440f25fa2b77de6e0f70479c8d7
Mustafa Sencer Özcan <32759850+mustafasencer@users.noreply.github.com>
2020-05-20 10:39:03 +0300

    - added array and string algorithm problems

---
+++ algorithms/array/3_sum.py
@@ -0,0 +1,14 @@
+"""
+    Created by Mustafa Sencer Özcan on 20.05.2020.
+"""
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        pass
+
+
+if __name__ == '__main__':
+    result = Solution().threeSum([-1, 0, 1, 2, -1, -4])
+    print(result)
