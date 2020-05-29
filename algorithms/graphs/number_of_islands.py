08ecb1e799103a7aeda63f993b2db7707accea3a
sencer.ozcan <Ruman123>
2020-05-29 17:24:07 +0300

    - improvements on new algorithms - graph algorithms started.

---
+++ algorithms/graphs/number_of_islands.py
@@ -0,0 +1,14 @@
+"""
+    Created by Mustafa Sencer Özcan on 27.05.2020.
+"""
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    result = Solution().numIslands()
+    print(result)
