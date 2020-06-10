bc058b330abd71521be9d8badcad0248198b20d5
Mustafa Sencer Özcan <32759850+mustafasencer@users.noreply.github.com>
2020-06-10 23:27:01 +0300

    - added course_schedule.py - added non_overlapping_intervals.py

---
+++ algorithms/intervals/non_overlapping_intervals.py
@@ -0,0 +1,11 @@
+"""
+    Created by Mustafa Sencer Özcan on 22.05.2020.
+
+    Given a collection of intervals, find the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+"""
+from typing import List
+
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        pass
