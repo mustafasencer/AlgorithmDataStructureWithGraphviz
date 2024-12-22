4dfe1b79e052bb763c27239c8d0c7642b6e990ad
mustafasencero <m.sencerozcan@gmail.com>
2024-12-22 13:11:36 +0300

    deps: update deps

--- visualization/graph.py
+++ visualization/graph.py
@@ -1,65 +1,65 @@
-from graphviz.base import DiGraphImpl
+from graphviz import Digraph as DiGraphImpl
 
 
 class GraphDrawer:
-    def __init__(self):
-        self.digraph = DiGraphImpl(format="png")
-        self.key_value_pairs = []
-        self._key = None
-        self.source = None
-        self.edges = []
+	def __init__(self):
+		self.digraph = DiGraphImpl(format='png')
+		self.key_value_pairs = []
+		self._key = None
+		self.source = None
+		self.edges = []
 
-    def get_key(self, value):
-        key = self.key
-        self.key_value_pairs.append((key, value))
-        return key
+	def get_key(self, value):
+		key = self.key
+		self.key_value_pairs.append((key, value))
+		return key
 
-    def get_source(self):
-        if self.key_value_pairs:
-            key, value = self.key_value_pairs.pop(0)
-            self.source = key
-            return key
-        key = self.key
-        self.source = key
-        return key
+	def get_source(self):
+		if self.key_value_pairs:
+			key, value = self.key_value_pairs.pop(0)
+			self.source = key
+			return key
+		key = self.key
+		self.source = key
+		return key
 
-    def create_edges(self):
-        for source, destination in self.edges:
-            self.digraph.add_edge(source, destination)
+	def create_edges(self):
+		for source, destination in self.edges:
+			self.digraph.add_edge(source, destination)
 
-    def create_node(self, value):
-        source = self.get_source()
-        self.digraph.add_node(str(source), str(value) if value else "null")
+	def create_node(self, value):
+		source = self.get_source()
+		self.digraph.add_node(str(source), str(value) if value else 'null')
 
-    def append_edges(self, root):
-        if root.left:
-            destination = self.get_key(root.left.val)
-            self.edges.append((str(self.source), str(destination)))
-        if root.right:
-            destination = self.get_key(root.right.val)
-            self.edges.append((str(self.source), str(destination)))
+	def append_edges(self, root):
+		if root.left:
+			destination = self.get_key(root.left.val)
+			self.edges.append((str(self.source), str(destination)))
+		if root.right:
+			destination = self.get_key(root.right.val)
+			self.edges.append((str(self.source), str(destination)))
 
-    @property
-    def key(self):
-        if not self._key:
-            self._key = 0
-        self._key += 1
-        return self._key
+	@property
+	def key(self):
+		if not self._key:
+			self._key = 0
+		self._key += 1
+		return self._key
 
-    def create_nodes(self, root):
-        if not root:
-            raise ValueError("Tree is empty!")
-        _result, queue = [], [root]
-        while len(queue) > 0:
-            node = queue.pop(0)
-            self.create_node(node.val)
-            self.append_edges(node)
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-        self.create_edges()
+	def create_nodes(self, root):
+		if not root:
+			raise ValueError('Tree is empty!')
+		_, queue = [], [root]
+		while len(queue) > 0:
+			node = queue.pop(0)
+			self.create_node(node.val)
+			self.append_edges(node)
+			if node.left:
+				queue.append(node.left)
+			if node.right:
+				queue.append(node.right)
+		self.create_edges()
 
-    def visualize(self, graph):
-        self.create_nodes(graph)
-        self.digraph.view()
+	def visualize(self, graph):
+		self.create_nodes(graph)
+		self.digraph.view()
